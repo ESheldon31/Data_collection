47e07dc21ee5b12c154dbdae464f82f1aef7fd3d
Shelduck <e.l.sheldon@icloud.com>
2022-04-22 15:48:09 +0200

    tidies get_frequency_and_phrases method

--- coll_scraper.py
+++ coll_scraper.py
@@ -1,4 +1,6 @@
-from scraper_module import CollocationsScraper
+#%%
+
+from collocations_module import CollocationsScraper
 
 def web_scraper_collocations():
     search_term = 'libertad'
@@ -6,14 +8,18 @@ def web_scraper_collocations():
     try:
         scraper.search('//input[@id="query"]')
         scraper.create_dict()
-        scraper.get_frequency(word_class='adj')
-        scraper.get_phrases(word_class='adj')
-        scraper.get_frequency(word_class='v')
-        scraper.get_phrases(word_class='v')
+        scraper.get_frequency_and_phrases(word_class='adj', dict_key='adj_phrases', url_mode='')
+        scraper.get_frequency_and_phrases(word_class='adj', dict_key='adj_rank-word-frequency', url_mode='?mode=frequency')
+        scraper.get_frequency_and_phrases(word_class='v', dict_key='v_phrases', url_mode='')
+        scraper.get_frequency_and_phrases(word_class='v', dict_key='v_rank-word-frequency', url_mode='?mode=frequency')
+        scraper.get_infinitives()
+        # scraper.get_frequency(word_class='v')
+        # scraper.get_phrases(word_class='v')
         #scraper.collate_data()
         scraper.download_raw_data(file_name='raw_data_coll')
-        time.sleep(2)
+        #time.sleep(2)
         # scraper.click_button('//span[@class="gl-new-dropdown-button-text"]')
         # scraper.click_button('//*[@class="gl-new-dropdown-item"][6]')
     finally: scraper.quit()
 web_scraper_collocations()
+# %%
