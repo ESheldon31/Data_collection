09e86a293352ba6237411c07fd3d328581fa8eb0
Shelduck <e.l.sheldon@icloud.com>
2022-04-22 15:46:36 +0200

    creates collect_info() method

--- lego_module.py
+++ lego_module.py
@@ -1,22 +1,21 @@
+#%%
 from scraper_module import Scraper
 from bs4 import BeautifulSoup as bs
 
 class LegoScraper(Scraper):
-    def __init__(self):
+    def __init__(self, url, search_term, headless=False):
         self.name_list = []
         self.date_list = []
         self.creator_list =[]
         self.num_supporters_list = []
         self.num_days_remaining_list = []
-        super().__init__(self)
+        super().__init__(url, search_term, headless=False)
     
-    def get_info_from_java(self):
-        for link in self.link_list:
-            self.driver.get(link)
-            soup = bs(self.driver.page_source, 'html.parser')
-            numbers = soup.findAll('div', class_= "count")
-            self.num_supporters_list.append(numbers[0].text)
-            self.num_days_remaining_list.append(numbers[1].text)
+    def get_supporters_days_remaining(self):
+        soup = bs(self.driver.page_source, 'html.parser')
+        numbers = soup.findAll('div', class_= "count")
+        self.num_supporters_list.append(numbers[0].text)
+        self.num_days_remaining_list.append(numbers[1].text)
     
     def create_id(self):
         self.id_list = []
@@ -28,24 +27,7 @@ class LegoScraper(Scraper):
         self.click_button(XPATH1)
         self.click_button(XPATH2)
 
-    def get_info_from_html(self):
-        self.name_list = []
-        self.date_list = []
-        self.creator_list =[]
-        
-        for link in self.link_list:
-            self.get_html(link)
-            name = self.soup.find('h1').text
-            self.name_list.append(name)
-
-            date = self.soup.find('span', {"class":"published-date"}).text
-            self.date_list.append(date)
-
-            creator_name = self.soup.find('a', {'data-axl':"alias"}).text
-            self.creator_list.append(creator_name)
-
-    #new 
-    def get_info_from_html(self, link):
+    def get_name_date_creator(self, link):
         self.get_html(link)
         name = self.soup.find('h1').text
         self.name_list.append(name)
@@ -57,14 +39,24 @@ class LegoScraper(Scraper):
         self.creator_list.append(creator_name)
 
     def collate_info(self):
-        self.info = {"id": self.link_id,
-                "uuid": self.link_uuid,
+        self.info = {
+            # "id": self.link_id,
+            #     "uuid": self.link_uuid,
                 "URL": self.link_list,
                 "idea_name": self.name_list,
                 "date": self.date_list,
                 "creator": self.creator_list,
                 "number_of_supporters": self.num_supporters_list,
-                "number_of_days_remaining": self.num_days_remaining_list,
-                "image_links": self.img_list}
+                "number_of_days_remaining": self.num_days_remaining_list}
+                #"image_links": self.img_list}
         #return self.info
+        print(self.info)
 
+    def collect_info(self):
+        for link in self.link_list:
+            self.open_url(link)
+            #self.get_html(link)
+            self.get_name_date_creator(link)
+            self.get_supporters_days_remaining()
+
+# %%
--- lego_scraper.py
+++ lego_scraper.py
@@ -1,4 +1,5 @@
-from scraper_module import LegoScraper
+#%%
+from lego_module import LegoScraper
 
 if __name__ == '__main__': 
 
@@ -8,18 +9,15 @@ if __name__ == '__main__':
         scraper = LegoScraper('https://ideas.lego.com', search_term)
         try:
             scraper.accept_cookies(frame_id=None, XPATH= '//button[@aria-label="Reject cookies"]')
-            #scraper.explore_product_ideas('//a[@class="sub-menu"][1]', '//div[@class="header-link"][1]')
             scraper.search('//input[@name="query"]')
             scraper.get_list_links('//*[@id="search_results"]', './div')
-            #time.sleep(2)
+
             # scraper.get_img_links(XPATH_main_image='//div[@class="image-sizing-wrapper"]', XPATH_thumbnail_container='//div[@class="thumbnails-tray"]', XPATH_thumbnails='./div')
-            scraper.create_uuid(scraper.link_list)
-            # scraper.create_id()
-            #scraper.get_html()
-            # scraper.get_info_from_html()
-            scraper.get_info_from_java()
+            #scraper.create_uuid(scraper.link_list)
+
+            scraper.collect_info()
             # #scraper.get_info()
-            # scraper.collate_info()
+            scraper.collate_info()
             # scraper.download_raw_data()
             #scraper.download_images()
             #scraper.create_uuid()
@@ -36,3 +34,5 @@ if __name__ == '__main__':
     web_scraper_lego()
     
 
+
+# %%
