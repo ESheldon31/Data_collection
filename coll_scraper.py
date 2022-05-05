8b5c14c2ab95edca980e5ebc2ffcd1f2fa2193a4
Shelduck <e.l.sheldon@icloud.com>
2022-05-05 18:02:38 +0200

    tidies

--- coll_scraper.py
+++ coll_scraper.py
@@ -5,21 +5,6 @@ from collocations_module import CollocationsScraper
 def web_scraper_collocations():
     search_term = 'libertad'
     scraper = CollocationsScraper('https://inspirassion.com/es/', search_term)
-    try:
-        scraper.search('//input[@id="query"]')
-        scraper.create_dict()
-        scraper.get_frequency_and_phrases(word_class='adj', dict_key='adj_phrases', url_mode='')
-        scraper.get_frequency_and_phrases(word_class='adj', dict_key='adj_rank-word-frequency', url_mode='?mode=frequency')
-        scraper.get_frequency_and_phrases(word_class='v', dict_key='v_phrases', url_mode='')
-        scraper.get_frequency_and_phrases(word_class='v', dict_key='v_rank-word-frequency', url_mode='?mode=frequency')
-        scraper.get_infinitives()
-        # scraper.get_frequency(word_class='v')
-        # scraper.get_phrases(word_class='v')
-        #scraper.collate_data()
-        scraper.download_raw_data(file_name='raw_data_coll')
-        #time.sleep(2)
-        # scraper.click_button('//span[@class="gl-new-dropdown-button-text"]')
-        # scraper.click_button('//*[@class="gl-new-dropdown-item"][6]')
-    finally: scraper.quit()
+    scraper.scraping_now()
 web_scraper_collocations()
 # %%
--- collocations_module.py
+++ collocations_module.py
@@ -4,16 +4,19 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 class CollocationsScraper(Scraper):
-    def create_dict(self):
+    def __init__(self, url, search_term, headless=False):
+        super().__init__(url, search_term, headless)
         self.info = {
-          "id": self.id_list,
+                #"id": self.id_list,
         #         "uuid": self.link_uuid,
                 "adj_rank-word-frequency": [],
                 "adj_phrases": [],
                 "verb_rank-word-frequency": [],
                 "infinitive_verb": [],
                 "verb_phrases": []}
-        #print(self.info)
+    # def create_dict(self):
+
+    #     #print(self.info)
 
     def get_words(self):        
         list_words = []
@@ -22,10 +25,10 @@ class CollocationsScraper(Scraper):
             self.list_words.append(word.text)
         return list_words
     
-    def create_id(self, word_list, word_class):
-        for i in range(len(word_list)):
-            id = f'{self.search_term}.{word_class}.{i}'
-            self.id_list.append(id)
+    # def create_id(self, word_list, word_class):
+    #     for i in range(len(word_list)):
+    #         id = f'{self.search_term}.{word_class}.{i}'
+    #         self.id_list.append(id)
             
     def get_infinitives(self):
         list_words = self.get_words()
@@ -55,11 +58,26 @@ class CollocationsScraper(Scraper):
     def get_frequency_and_phrases(self, word_class, dict_key, url_mode):
         temp_list = []
         new_url= f'{self.url}{word_class}/{self.search_term}{url_mode}'
-        self.get_html(new_url)
-        sentences = self.soup.findAll('li', {"class": "btn-result list-group-item list-group-item-action"})
+        self.open_url(new_url)
+        sentences = self.find_all_in_html('li', 'class', 'btn-result list-group-item list-group-item-action')
         for sentence in sentences:
             temp_list.append(sentence.text)
         self.info[dict_key] = temp_list      
 
+    def scraping_now(self):
+        try:
+            self.search('//input[@id="query"]')
+            #self.create_dict()
+            self.get_frequency_and_phrases(word_class='adj', dict_key='adj_phrases', url_mode='')
+            self.get_frequency_and_phrases(word_class='adj', dict_key='adj_rank-word-frequency', url_mode='?mode=frequency')
+            self.get_frequency_and_phrases(word_class='v', dict_key='v_phrases', url_mode='')
+            self.get_frequency_and_phrases(word_class='v', dict_key='v_rank-word-frequency', url_mode='?mode=frequency')
+            self.get_infinitives()
+            print(self.info)
+            # scraper.get_frequency(word_class='v')
+            # scraper.get_phrases(word_class='v')
+            #scraper.collate_data()
+            self.download_raw_data(file_name='raw_data_coll')
+        finally: self.quit()
 
 # %%
