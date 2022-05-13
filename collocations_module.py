3c39e047122406da8d73a073fba67f856e5198bd
Shelduck <e.l.sheldon@icloud.com>
2022-05-13 13:00:50 +0200

    updates ToDo list

--- collocations_module.py
+++ collocations_module.py
@@ -2,27 +2,41 @@
 from scraper_module import Scraper
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from data_template import CollocationsData
+
+#ToDo: tidy up
+#ToDo: Docstrings
+#ToDo: fix get_words and get_infinitives
+#ToDo: create methods for id and uuid
+#ToDo: create config file 
 
 class CollocationsScraper(Scraper):
     def __init__(self, url, search_term, headless=False):
         super().__init__(url, search_term, headless)
-        self.info = {
-                #"id": self.id_list,
-        #         "uuid": self.link_uuid,
-                "adj_rank-word-frequency": [],
-                "adj_phrases": [],
-                "verb_rank-word-frequency": [],
-                "infinitive_verb": [],
-                "verb_phrases": []}
-    # def create_dict(self):
+    #     self.info = {
+    #             #"id": self.id_list,
+    #     #         "uuid": self.link_uuid,
+    #             "adj_rank-word-frequency": [],
+    #             "adj_phrases": [],
+    #             "verb_rank-word-frequency": [],
+    #             "infinitive_verb": [],
+    #             "verb_phrases": []}
+    # # def create_dict(self):
 
     #     #print(self.info)
 
-    def get_words(self):        
+    def get_words(self):
+        url = f'https://inspirassion.com/es/v/{self.search_term}'
+        self.driver.get(url)        
         list_words = []
-        words = self.driver.find_elements(By.XPATH, '//span[@class="  result font-weight-bold text-success "]')
+        try:
+            words = self.driver.find_elements(By.XPATH, '//span[@class=" result  "]')
+            print(words)
+        except NoSuchElementException:
+            print('Element not found.')
         for word in words:
-            self.list_words.append(word.text)
+            list_words.append(word.text)
+        print(list_words)
         return list_words
     
     # def create_id(self, word_list, word_class):
@@ -31,53 +45,83 @@ class CollocationsScraper(Scraper):
     #         self.id_list.append(id)
             
     def get_infinitives(self):
+        #ToDo: investigate why this is not working
         list_words = self.get_words()
-        list_infinitives = []
-        for word in list_words:
-            dictionary_url = f'https://es.thefreedictionary.com/{word}'
-            self.driver.get(dictionary_url)
-            try:
-                accept_cookies = self.driver.find_element(By.XPATH, '/html/body/div[5]/div/div[1]/div/div/div[2]/a[1]')
-                accept_cookies.click()
-            except NoSuchElementException:
-                pass
-            try:
-                infinitive = self.driver.find_element(By.tag_name, 'h1')
-                list_infinitives.append(infinitive.text)
-            except NoSuchElementException:
-                list_infinitives.append('N/A')
-            self.driver.close()
-        self.info["infinitive_verbs"] = list_infinitives
+        if bool(list_words) == True:
+            list_infinitives = []
+            for word in list_words:
+                original_window = self.driver.current_window_handle
+                dictionary_url = f'https://es.thefreedictionary.com/{word}'
+                self.driver.get(dictionary_url)
+                try:
+                    accept_cookies = self.driver.find_element(By.XPATH, '/html/body/div[5]/div/div[1]/div/div/div[2]/a[1]')
+                    accept_cookies.click()
+                except NoSuchElementException:
+                    pass
+                try:
+                    infinitive = self.driver.find_element(By.TAG_NAME, 'h1')
+                    list_infinitives.append(infinitive.text)
+                except NoSuchElementException:
+                    list_infinitives.append('N/A')
+                self.driver.close()
+                self.driver.switch_to.window(original_window)
+            return list_infinitives
+        else: print('list_words does not exist')
     
-    def choose_mode(self):
-        mode_dict = {
-            'word_class': ['adj', 'v'],
-            'url_mode': ['', '?mode=frequency']
-        }
 
-    def get_frequency_and_phrases(self, word_class, dict_key, url_mode):
+    def get_frequency_and_phrases(self, word_class, url_mode, tag, attribute, attribute_name):
         temp_list = []
         new_url= f'{self.url}{word_class}/{self.search_term}{url_mode}'
         self.open_url(new_url)
-        sentences = self.find_all_in_html('li', 'class', 'btn-result list-group-item list-group-item-action')
-        for sentence in sentences:
-            temp_list.append(sentence.text)
-        self.info[dict_key] = temp_list      
+        element = self.find_all_in_html(tag, attribute, attribute_name)
+        for element in element:
+            temp_list.append(element.text)
+        return temp_list      
+
+    def collect_info(self):
+        coll_data = CollocationsData(
+            uuid_list=[],
+            id_list=[],
+            img_list=None,
+            link_list=None, 
+            adj_phrases=[],
+            adj_rank_word_frequency=[],
+            verb_phrases=[],
+            verb_rank_word_frequency=[],
+            infinitive_verb=[])
+
+        coll_data.adj_phrases = self.get_frequency_and_phrases(word_class='adj', 
+                                                            url_mode='', 
+                                                            tag='p', 
+                                                            attribute='class', 
+                                                            attribute_name='btn-result text-start border-gray-300 border-bottom p-4 bg-hover-light-dark m-0')
+        coll_data.adj_rank_word_frequency = self.get_frequency_and_phrases(word_class='adj', url_mode='?mode=frequency', tag='div', attribute='class', 
+                                                            attribute_name='d-flex flex-wrap justify-content-between align-items-center px-3')
+        coll_data.verb_phrases = self.get_frequency_and_phrases(word_class='v', url_mode='', tag='p', 
+                                                            attribute='class', 
+                                                            attribute_name='btn-result text-start border-gray-300 border-bottom p-4 bg-hover-light-dark m-0')
+        coll_data.infinitive_verb = self.get_infinitives()
+        coll_data.verb_rank_word_frequency = self.get_frequency_and_phrases(word_class='v', url_mode='?mode=frequency', tag='div', attribute='class', 
+                                                            attribute_name='d-flex flex-wrap justify-content-between align-items-center px-3')
+        
+        return coll_data
 
     def scraping_now(self):
         try:
+            self.close_pop_up('//div[@class="drift-controller-icon--close"]')
             self.search('//input[@id="query"]')
+            coll_data = self.collect_info()
             #self.create_dict()
-            self.get_frequency_and_phrases(word_class='adj', dict_key='adj_phrases', url_mode='')
-            self.get_frequency_and_phrases(word_class='adj', dict_key='adj_rank-word-frequency', url_mode='?mode=frequency')
-            self.get_frequency_and_phrases(word_class='v', dict_key='v_phrases', url_mode='')
-            self.get_frequency_and_phrases(word_class='v', dict_key='v_rank-word-frequency', url_mode='?mode=frequency')
-            self.get_infinitives()
-            print(self.info)
+            # self.get_frequency_and_phrases(word_class='adj', dict_key='adj_phrases', url_mode='')
+            # self.get_frequency_and_phrases(word_class='adj', dict_key='adj_rank-word-frequency', url_mode='?mode=frequency')
+            # self.get_frequency_and_phrases(word_class='v', dict_key='v_phrases', url_mode='')
+            # self.get_frequency_and_phrases(word_class='v', dict_key='v_rank-word-frequency', url_mode='?mode=frequency')
+            # self.get_infinitives()
+            # print(self.info)
             # scraper.get_frequency(word_class='v')
             # scraper.get_phrases(word_class='v')
             #scraper.collate_data()
-            self.download_raw_data(file_name='raw_data_coll')
+            self.download_raw_data(coll_data, file_name='raw_data_coll')
         finally: self.quit()
 
 # %%
--- lego_module.py
+++ lego_module.py
@@ -5,11 +5,39 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from data_template import LegoData
 
+#ToDo: config file with XPATHs stored in dict
+#ToDo: type hints
+# ToDo: add details of public methods in class docstring
+'''
+This module contains a child scraper class and its methods.
+
+'''
 class LegoScraper(Scraper):
+    '''
+    A child class of Scraper designed specifically to scrape the LegoIdeas website.
+
+    '''
+    
     def __init__(self, url, search_term, headless=False):
-        super().__init__(url, search_term, headless=False)
+        '''
+        calls the necessary attributes from the parent class
+        '''
+        super().__init__(url, search_term, headless)
 
     def get_links(self, XPATH_container, XPATH_search_results):
+        '''
+        collects the links to each idea page from the search results 
+        uses infinite_scroll to ensure links for all results are collected
+        
+        Parameters:
+            XPATH_container: str
+                XPATH for the container that holds the search results
+            XPATH_search_results: str
+                XPATH for the individual search results
+                
+        Returns:
+            list of links 
+        '''
         self.scroll_down_bottom()
         try:
             self.see_more('//*[@id="search-more"]/a')
@@ -21,43 +49,117 @@ class LegoScraper(Scraper):
         return list_links
 
     def get_figures(self):
+        '''
+        collects the numerical information from webpage. 
+        
+        Returns: list
+            list of figures(str)
+            
+        '''
         numbers = self.find_all_in_html('div', 'class', 'count')
         return numbers
     
     def get_supporters(self):
+        '''
+        finds the number of supporters for each Lego idea
+
+        Returns : str
+            number of supporters
+
+        '''
         supporters = (self.get_figures())[0].text
         stripped_supporters = supporters.strip()
         return stripped_supporters
 
     def get_days_remaining(self):
+        '''
+        finds the number of days the idea has left on the website. 
+        This number increases with increasing numbers of supporters.
+
+        Returns : str
+            number of days remaining
+
+        '''
         days_remaining = (self.get_figures())[1].text
         stripped_days_remaining = days_remaining.strip()
         return stripped_days_remaining
 
     def get_name(self, link):
+        '''
+        finds the idea name for each Lego idea
+        
+        Parameters:
+        link : str
+            url of the Lego idea result page
+
+        Returns : str
+            idea name
+
+        '''
         name = self.find_in_html(link, 'h1', None, None)
         return name
 
     def get_date(self, link):
+        '''
+        for each Lego idea, finds the date it was submitted to the website
+        
+        Parameters:
+        link : str
+            url of the Lego idea result page
+
+        Returns : str
+            date
+            
+        '''
         date = self.find_in_html(link, 'span', 'class', 'published-date')
         return date
 
     def get_creator_name(self, link):
+        '''
+        finds the creator's name for each Lego idea
+        
+        Parameters:
+        link : str
+            url of the Lego idea result page
+
+        Returns : str
+            creator's name
+
+        '''
         creator_name = self.find_in_html(link, 'a', 'data-axl', 'alias')
         stripped_creator_name = creator_name.strip()
         return stripped_creator_name
 
     def create_id(self, link):
+        '''
+        creates user-friendly ID for each Lego idea
+        
+        Parameters:
+            link : str
+                url of the Lego idea result page
+
+        Returns : str
+            ID
+
+     '''
         name = self.get_name(link)
         stripped_creator_name = self.get_creator_name(link)
         ID = f'{name}.{stripped_creator_name}'
         return ID
     
     def explore_product_ideas(self, XPATH1, XPATH2):
+        #not used. Remove?
         self.click_button(XPATH1)
         self.click_button(XPATH2)
 
     def collect_info(self):
+        '''
+        collects list of links, creates instance of dataclass, for each link appends data to list in dataclass
+
+        Returns:
+            instance of dataclass with all data appended
+
+        '''
         link_list = self.get_links('//*[@id="search_results"]', './div')
         lego_data = LegoData(
             uuid_list=[], 
@@ -87,6 +189,9 @@ class LegoScraper(Scraper):
         return(lego_data)
 
     def scraping_now(self):
+        '''
+        runs the various methods to accept cookies, search, collect and download data and images
+        '''
         try:
             self.accept_cookies(frame_id=None, XPATH= '//button[@aria-label="Reject cookies"]')
             self.search('//input[@name="query"]')
